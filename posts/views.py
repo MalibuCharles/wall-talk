32dbb61b369ef87b57317a73ab1fb551b5fc8ed9
Berthonise Charles <berthonisecharles@Berthonises-MacBook-Air-2.local>
2022-05-07 20:48:05 -0400

    step 11

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+
+
+from .models import Post
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
     return HttpResponse("<h1>Hello World</h1>")
 
 def post_detail_view(request, post_id, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse(f"<h1>Hello {post_id}</h1>")
+    try:
+        obj = Post.objects.get(id=post_id)
+    except:
+        raise Http404
+    return HttpResponse(f"<h1>Hello {post_id} - {obj.content}</h1>")
