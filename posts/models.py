b859d0fba11f64f7178f61fd0b1d217a4961a67d
Berthonise Charles <berthonisecharles@Berthonises-MacBook-Air-2.local>
2022-05-07 20:02:29 -0400

    step 8

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='image/', blank=True, null=True) 
