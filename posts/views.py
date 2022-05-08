d8de963870f19d280221819f38bc8049fb0ae870
Berthonise Charles <berthonisecharles@Berthonises-MacBook-Air-2.local>
2022-05-07 21:00:12 -0400

    step 12

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 
@@ -9,8 +9,19 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
 
 def post_detail_view(request, post_id, *args, **kwargs):
+    """
+    REST API VIEW 
+    Consume By JS
+    return json data
+    """
+    data = {
+     "id": post_id,
+    }
+    status = 200
     try:
         obj = Post.objects.get(id=post_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1>Hello {post_id} - {obj.content}</h1>")
+        data['message'] = "Not found" 
+        status = 404     
+    return JsonResponse(data, status=status)
